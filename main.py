45db18ca77ee01b6c1774d3e2a1f7544a5c81af8
RamLi06 <ramon.rocha.809@gmail.com>
2026-01-24 11:20:25 -0300

    Bugfix-Ajustes na probabilidade

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def main():
 		from components.jumpscare import run_continuous
 		gif = sys.argv[2] if len(sys.argv) > 2 else SUSTOS['Chica']['gif']
 		sound = sys.argv[3] if len(sys.argv) > 3 else SUSTOS['Chica']['sound']
-		run_continuous(gif_path=gif, sound_path=sound, probability=1.0, interval_seconds=1.0)
+		run_continuous(gif_path=gif, sound_path=sound, probability=0.01, interval_seconds=1.0)
 		return
 	app = QtWidgets.QApplication(sys.argv)
 	window = MainWindow()
